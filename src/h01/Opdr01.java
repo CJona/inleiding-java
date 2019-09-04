e505d2a8b074a6c6106ff3bddc03935a8ab8f801
CJona <54817036+CJona@users.noreply.github.com>
2019-09-04 08:56:57 +0200

    opdr01

--- src/h01/Opdr01.java
+++ src/h01/Opdr01.java
@@ -1,6 +1,20 @@
 package h01;
 
+import javax.swing.*;
 import java.applet.Applet;
+import java.awt.*;
 
 public class Opdr01 extends Applet {
+    public void init() {
+        setBackground(Color.white);
+
+
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.blue);
+        g.drawString("Jona", 50, 60);
+        g.setColor(Color.red);
+        g.drawString("Cameron", 30, 40);
+    }
 }
