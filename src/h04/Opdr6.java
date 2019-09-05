3e31a233e53c56aef33346c29b05254f526025ce
CJona <54817036+CJona@users.noreply.github.com>
2019-09-05 14:13:18 +0200

    Opdr6 Stop Licht

---
+++ src/h04/Opdr6.java
@@ -0,0 +1,34 @@
+package h04;
+import java.awt.*;
+import java.applet.*;
+
+public class Opdr6 extends Applet {
+
+    public void init() {
+        setBackground(Color.WHITE);
+    }
+
+    public void paint(Graphics g) {
+        // Bord
+        g.setColor(Color.BLACK);
+        g.fillRoundRect(40, 20, 40, 80, 20, 20);
+        // Paal
+        g.fillRect(55, 80, 10, 100 );
+        // Rood licht
+        g.setColor(Color.red);
+        g.fillOval(50, 25, 20, 20);
+        // Oranje licht
+        g.setColor(Color.orange);
+        g.fillOval(50, 50, 20, 20);
+        // Groen licht
+        g.setColor(Color.green);
+        g.fillOval(50, 75, 20, 20);
+
+        // Handtekening
+        g.setColor(Color.RED);
+        g.drawString("Jona made This", 100, 250);
+
+
+
+    }
+}
