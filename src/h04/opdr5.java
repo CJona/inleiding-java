16aae567a0ecae1dfa4658fa8dc6c28ef1009509
CJona <54817036+CJona@users.noreply.github.com>
2019-09-05 08:42:26 +0200

    Opdr 5 ( FillArc + ellips )

---
+++ src/h04/opdr5.java
@@ -0,0 +1,20 @@
+package h04;
+import java.awt.*;
+import java.applet.*;
+
+public class opdr5 extends Applet {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    public void init() {
+        setBackground(Color.blue);
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.yellow);
+        g.fillArc(10, 10, 100, 100, 0, 80);
+    }
+}
