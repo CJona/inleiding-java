5e9875ee8fbe507500d5cfb38c53fd447c44e93e
CJona <54817036+CJona@users.noreply.github.com>
2019-09-06 13:25:11 +0200

    Opdr7 Dobbelsteen Vlak met 4 steentjes

---
+++ src/h04/Opdr7.java
@@ -0,0 +1,25 @@
+package h04;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdr7 extends Applet {
+    public void init(){
+    }
+
+    public void paint(Graphics g) {
+        setBackground(Color.BLACK);
+        // Vierkant
+        g.setColor(Color.GREEN);
+        g.drawRoundRect(50, 50, 250, 250, 20, 20);
+        // 4 puntjes in vierkant
+        g.fillOval(200, 100, 55, 55);
+        g.fillOval(100, 100, 55, 55);
+        g.fillOval(100, 200, 55, 55);
+        g.fillOval(200, 200, 55, 55);
+
+
+
+
+    }
+}
