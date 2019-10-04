c71df0d223d0e98153c0d70c319778c0f1d83ba6
CJona <54817036+CJona@users.noreply.github.com>
2019-10-04 14:37:57 +0200

    H12_3

--- src/H12/H12_3.java
+++ src/H12/H12_3.java
@@ -1,20 +1,41 @@
 package H12;
 
 
-import javax.swing.*;
 import java.applet.Applet;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
 
 public class H12_3 extends Applet {
     TextField[] tekstvak = new TextField[5];
     Button knop = new Button("ok");
 
     public void init() {
-        for (int i = 0; i < tekstvak.length; i++ ){
+        for (int i = 0; i < tekstvak.length; i++) {
+            tekstvak[i] = new TextField("", 20);
+            add(tekstvak[i]);
+        }
+        knop.addActionListener(new KnopListener());
+        add(knop);
+    }
+
+    class KnopListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            int[] getallen = new int[tekstvak.length];
+            for (int i = 0; i < tekstvak.length; i++) {
+                getallen[i] = Integer.parseInt(tekstvak[i].getText());
+            }
+            Arrays.sort(getallen);
+            for (int i = 0; i < tekstvak.length; i++) {
+                tekstvak[i].setText(""+getallen[i]);
+            }
 
         }
+    }
 
-
+    public void paint(Graphics g) {
 
     }
+
 }
