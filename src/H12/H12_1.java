6765116a7e9d6ca942e2d40818f7c0ee4b7aabdb
CJona <54817036+CJona@users.noreply.github.com>
2019-10-04 13:53:01 +0200

    H12_1

---
+++ src/H12/H12_1.java
@@ -0,0 +1,25 @@
+package H12;
+
+import java.applet.Applet;
+import java.awt.*;
+import java.util.*;
+
+public class H12_1 extends Applet {
+    int[] getallen = { 1, 33, 94, 4838, 93938, 12, 800, 24, 34020, 10};
+    int gemiddelde;
+
+    public void init() {
+        setBackground(Color.GREEN);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        for(int i = 0; i < getallen.length; i++) {
+            g.setFont(new Font("default", Font.BOLD, 14));
+            g.drawString("" + getallen[i], 10, 30+(i*15));
+            gemiddelde += getallen[i];
+        }
+        gemiddelde /= getallen.length;
+        g.drawString("Gemiddelde is " + gemiddelde, 50, 40);
+    }
+}
